8ee694549b88c43a42c3602c554a5e4760e516b6
Huzaifa622 <thuzaifa07@gmail.com>
2023-03-09 14:38:52 +0500

    commiting

---
+++ my.js
@@ -0,0 +1,104 @@
+// Objects
+// let employee={
+// name: "huzaifa",
+// salary: 10000,
+// company: "huzaifa company",
+// company2:"huzaifa second company"
+// }
+
+// console.log(employee.name);
+// console.log(employee.salary);
+// console.log(employee['company']);
+
+// Arrays
+
+// let arr=[2,1,4,5,8,9];
+// console.log(arr);
+// console.log(arr.sort());
+// console.log(arr.length);
+// arr.push("this is me");
+
+// Array object
+
+// let a=new Array(1,5,3,7,5,8,"huzaifa",true);
+
+// a=a.sort();
+// console.log(a);
+
+// let b=new Array(23);
+// console.log(b);
+
+// functions
+
+// function welcome(name , greet){
+//     console.log(name + " " + greet);
+// console.log(name + " welcome today");
+
+// }
+
+// let greeting="how are you?"
+// let myName="huzaifa";
+
+// welcome(myName,greeting);
+
+// function conditions(num1,num2){
+// let yes="first number is greater";
+// let no="second number is greater";
+// if(num1>num2){
+//     console.log(yes);
+// }else{
+//     console.log(no);
+// }
+// }
+
+// let a=54;
+// let b=53;
+
+// conditions(a,b);
+// alert,prompt.confirm
+
+// alert("error");
+
+// let age=prompt("what is your age","guest");
+// if(age>=18){
+//     console.log("you are allowed in club");
+    
+// }else{
+//     console.log("you are not allowed in club");    
+// }
+
+// let name=confirm("Done!");
+// console.log(name);
+
+// Loops
+
+// for(let i=0; i<4; i++){
+// console.log(i);
+// }
+
+// let names=["Huzaifa","Owais","Umar","Bilal"];
+
+// for (let index = 0; index < names.length; index++) {
+//     console.log(names[index]);
+    
+// }
+
+// names.forEach(element => {
+//     console.log(element);
+// });
+
+// for (elements of names){
+//     console.log(elements);
+// }
+
+// let employee={
+// name:"Huzaifa",
+// salary: 2000,
+// Channel:"huzaifa"
+// }
+
+// for (key in employee) {
+//   console.log(`The ${key} of employee is ${employee[key]}`);
+// }
+
+// while and do while loop are same as c and java
